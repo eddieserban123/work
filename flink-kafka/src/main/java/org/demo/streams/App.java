6115cfd3c66f6d0ecb03d19a5df4917f2b49fe65
eddie <eddie.serban>
2018-01-29 18:54:57 +0200

    add kafka streaming

---
+++ flink-kafka/src/main/java/org/demo/streams/App.java
@@ -0,0 +1,16 @@
+package org.demo.streams;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment()
+    }
+}
