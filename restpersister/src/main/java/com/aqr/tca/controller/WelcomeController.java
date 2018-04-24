310f1626916d5c59938e8f2e3202c5421ec130d9
eddie <eddie.serban>
2018-04-24 16:51:07 +0300

    add missing files

--- restpersister/src/main/java/com/aqr/tca/controller/WelcomeController.java
+++ restpersister/src/main/java/com/aqr/tca/controller/WelcomeController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+
 @Controller
 public class WelcomeController {
 
@@ -14,7 +16,8 @@ public class WelcomeController {
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("topics", topicService.getTopics().getTopics());  // i know is wrong
+     //   model.addAttribute("topics", topicService.getTopics().getTopics());  // i know is wrong
+        model.addAttribute("topics", Arrays.asList("topic1","topic","topic3","topic4"));
         return "index";
     }
 }
