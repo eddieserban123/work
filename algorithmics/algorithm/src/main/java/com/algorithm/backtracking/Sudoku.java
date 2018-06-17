15dab09a902ace52430cf6c31bf406aa9703b7f0
Eduard Serban <eddie.serban@gmail.com>
2018-06-17 23:17:30 +0300

    add sudoku

---
+++ algorithmics/algorithm/src/main/java/com/algorithm/backtracking/Sudoku.java
@@ -0,0 +1,30 @@
+package com.algorithm.backtracking;
+
+public class Sudoku {
+
+   private static int SIZE= 9;
+
+   private static int mat[][]= {
+        {3,0,6,5,0,8,4,0,0},
+        {5,2,0,0,0,0,0,0,0},
+        {0,8,7,0,0,0,0,3,1},
+        {0,0,3,0,1,0,0,8,0},
+        {9,0,0,8,6,3,0,0,5},
+        {0,5,0,0,9,0,6,0,0},
+        {1,3,0,0,0,0,2,5,0},
+        {0,0,0,0,0,0,0,7,4},
+        {0,0,5,2,0,6,3,0,0}
+   };
+
+   private static class Info {
+       public int i;
+       public int j;
+       
+   }
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
