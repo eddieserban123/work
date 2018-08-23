46c3fb4d4a218aeec824a40a89c49c3162ea15d3
eddie <eddie.serban>
2018-08-23 15:36:02 +0300

    add grovvyScript

--- java/validator/src/main/java/groovy/GroovyApp1.java
+++ java/validator/src/main/java/groovy/GroovyApp1.java
@@ -2,17 +2,13 @@ package groovy;
 
 import model.Trades;
 import util.BusinessHelper;
-
 import java.io.IOException;
 
-import static groovy.GroovyEvalJson.evalTrades;
-
-public class GroovyApp {
+public class GroovyApp1 {
 
     public static void main(String[] args) throws IOException {
-
         Trades trades = BusinessHelper.getTrades("2014-05-15");
-        evalTrades(trades);
+        GroovyEval.evalTrades(trades);
 
     }
 }
--- java/validator/src/main/java/groovy/GroovyEval.java
+++ java/validator/src/main/java/groovy/GroovyEval.java
@@ -1,56 +1,38 @@
 package groovy;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.deploy.util.StringUtils;
 import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
-import model.Trade;
+import groovy.lang.Script;
+import groovy.lang.Tuple;
 import model.Trades;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
-import java.util.Map;
 
 public class GroovyEval {
-
     private static Binding binding;
-    private static GroovyShell groovyShell;
-    private static List<String> differentTCAOpenPriceWithinAQRIDTradeDateScript;
+    private static GroovyShell shell;
+    private static Script scrpt = null;
 
     static {
-        binding = new Binding();
-        groovyShell = new GroovyShell(binding);
-
-        URL url = Thread.currentThread().getContextClassLoader().getResource("tradeRules.json");
-        File file = new File(url.getPath());
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, List<String>> map = null;
         try {
-            map = objectMapper.readValue(file, Map.class);
-
+            binding = new Binding();
+            shell = new GroovyShell(binding);
+            URL url = Thread.currentThread().getContextClassLoader().getResource("tradesRules.groovy");
+            scrpt = shell.parse(new File(url.getPath()));
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        Map<String,Object> res = (Map<String, Object>) map.get("DifferentTCAOpenPriceWithinAQRIDTradeDateRule");
-
-        differentTCAOpenPriceWithinAQRIDTradeDateScript = (List<String>)res.get("rule");
     }
-
 
     public GroovyEval() {
     }
 
     public static boolean evalTrades(Trades trades) {
-
         binding.setVariable("alltrades", trades);
-        Object result = groovyShell.evaluate(StringUtils.join(differentTCAOpenPriceWithinAQRIDTradeDateScript, "\n"));
-        System.out.println(result);
-        return true;
+        binding.setVariable("tools", scrpt);
+        Tuple ret = (Tuple) shell.evaluate("tools.validateTrades(alltrades)");
+        return (boolean) ret.get(0);
     }
-
-
 }
