bb7fde6701e381defd4e80cb2f3e75a0270e7ce8
eddie <eddie.serban>
2018-10-08 18:28:46 +0300

    add a new prj

---
+++ java/spring-boot/jpa-pure-demo/src/main/java/com/purejpa/demo/jpapuredemo/JpaPureDemoApplication.java
@@ -0,0 +1,13 @@
+package com.purejpa.demo.jpapuredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPureDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JpaPureDemoApplication.class, args);
+	}
+}
---
+++ java/spring-boot/jpa-pure-demo/src/test/java/com/purejpa/demo/jpapuredemo/JpaPureDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.purejpa.demo.jpapuredemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpaPureDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
