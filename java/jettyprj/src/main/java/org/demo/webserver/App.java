ed309721609cb7d05880f5db52a540e3ec1b53a4
Eduard Serban <eddie.serban@gmail.com>
2019-04-03 22:35:55 +0300

    add jetty webserver project

--- java/jettyprj/src/main/java/org/demo/webserver/App.java
+++ java/jettyprj/src/main/java/org/demo/webserver/App.java
@@ -1,13 +1,31 @@
 package org.demo.webserver;
 
+import org.demo.webserver.servlet.PriceServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
+
+import java.util.concurrent.Flow;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws Exception {
+        startServer();
+        
+    }
+
+    private static void startServer() throws Exception {
+        System.out.println("Hello World!");
+        Server server = new Server(8888);
+        ServletHandler servletHandler = new ServletHandler();
+        servletHandler.addServletWithMapping(PriceServlet.class, "/price");
+        server.setHandler(servletHandler);
+
+        server.start();
+        server.dumpStdErr();
+        server.join();
     }
 }
+
+
