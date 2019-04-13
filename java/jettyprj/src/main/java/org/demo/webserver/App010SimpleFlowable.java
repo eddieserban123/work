f006811a42577ff984bfa6eb0a0d54ea2977b18a
Eduard Serban <eddie.serban@gmail.com>
2019-04-13 15:39:09 +0300

    change structure

--- java/jettyprj/src/main/java/org/demo/webserver/App010SimpleFlowable.java
+++ java/jettyprj/src/main/java/org/demo/webserver/App010SimpleFlowable.java
@@ -8,7 +8,7 @@ import org.eclipse.jetty.server.Server;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-public class App01SimpleFlowable {
+public class App010SimpleFlowable {
     private static int PORT = 8888;
 
     public static void main(String[] args) throws Exception {
---
+++ java/jettyprj/src/main/java/org/demo/webserver/App040FlowableWithSubscriber.java
@@ -0,0 +1,78 @@
+package org.demo.webserver;
+
+import io.reactivex.Flowable;
+import org.apache.http.client.fluent.Request;
+import org.demo.webserver.server.MyServer;
+import org.eclipse.jetty.server.Server;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.io.IOException;
+import java.time.LocalTime;
+import java.util.concurrent.TimeUnit;
+
+/*
+https://github.com/ReactiveX/RxJava/wiki/Backpressure
+*/
+public class App040FlowableWithSubscriber {
+    private static int PORT = 8888;
+
+    public static void main(String[] args) throws Exception {
+        Server s = MyServer.start(PORT);
+
+        Flowable<Double> f1 = Flowable.interval(100, TimeUnit.MILLISECONDS).
+                map(v -> getPrice("/price")).
+                map(Double::parseDouble);
+
+        f1.subscribe(new MySubscriber());
+        s.join();
+    }
+
+
+    private static String getPrice(String url) throws IOException {
+        return Request.Get("http://localhost:" + PORT + url)
+                .execute()
+                .returnContent()
+                .asString()
+                .trim();
+    }
+}
+
+
+class MySubscriber implements Subscriber<Double> {
+    private final long INTERVAL = 1000;
+    Subscription subscription;
+    long oldTime;
+
+    @Override
+    public void onSubscribe(Subscription subscription) {
+        this.subscription = subscription;
+        oldTime = System.currentTimeMillis();
+        subscription.request(1000);
+    }
+
+    @Override
+    public void onNext(Double aDouble) {
+        try {
+            System.out.println(LocalTime.now() + " --> " + aDouble);
+            long diff = System.currentTimeMillis() - oldTime;
+            while (diff < INTERVAL) {
+                Thread.sleep(INTERVAL - diff);
+                diff = System.currentTimeMillis() - oldTime;
+            }
+            subscription.request(1);
+            oldTime = System.currentTimeMillis();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+
+    }
+
+    @Override
+    public void onComplete() {
+    }
+}
