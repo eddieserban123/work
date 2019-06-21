d5da201506b9f68da163082e92d5469d4f4040a8
Eddie Serban <eddie.serban@gmail.com>
2019-06-21 18:16:44 +0300

    add logger

--- java/jettyprj/src/main/java/org/demo/webserver/javarx/App020SimpleFlowable.java
+++ java/jettyprj/src/main/java/org/demo/webserver/javarx/App020SimpleFlowable.java
@@ -13,12 +13,24 @@ public class App020SimpleFlowable {
 
     public static void main(String[] args) throws Exception {
         Server s =  MyServer.start(PORT);
+        mapExample();
+
+        s.join();
+    }
+
+    private static void mapExample() {
         Flowable.interval(1000, TimeUnit.MILLISECONDS).
                 map(v-> getPrice("/price")).
                 map(Double::parseDouble).
                 blockingSubscribe(System.out::println);
-        s.join();
     }
+
+    private static void flatMapExample() {
+        Flowable.just("audi", "bmw", "opel").
+                flatMap(v-> getPrice("/price")).
+                blockingSubscribe(System.out::println);
+    }
+
 
 
     private static String getPrice(String url) throws IOException {
--- java/jettyprj/src/main/java/org/demo/webserver/javarx/App030ZipSimple.java
+++ java/jettyprj/src/main/java/org/demo/webserver/javarx/App030ZipSimple.java
@@ -2,6 +2,7 @@ package org.demo.webserver.javarx;
 
 import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
+import io.reactivex.schedulers.Schedulers;
 import org.apache.http.client.fluent.Request;
 import org.demo.webserver.server.MyServer;
 import org.eclipse.jetty.server.Server;
@@ -11,6 +12,8 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
+
+import static org.demo.webserver.javarx.Color.*;
 
 enum Color {
     RED,
@@ -25,21 +28,16 @@ public class App030ZipSimple {
     private static Logger logger = LoggerFactory.getLogger(App030ZipSimple.class);
 
     public static void main(String[] args) throws Exception {
-       // zip001Simple();
-        zip002Merge();
+         //zip001Simple();
+        //zip002Merge();
+        //zip003Concat();
 
     }
 
 
     private static void zip001Simple() throws IOException {
-        Flowable<String> f1 = Flowable.just("Peter", "Paul", "Marry", "Bob");
-
-        Flowable<String> f2 = Flowable.create(e -> {
-            for (int i = 0; i < 4; i++) {
-                e.onNext(Color.values()[ThreadLocalRandom.current().nextInt(4)].name());
-            }
-            e.onComplete();
-        }, BackpressureStrategy.MISSING);
+        Flowable<String> f1 = createFlowable(1, TimeUnit.MILLISECONDS, "Peter", "Paul", "Marry", "Bob");
+        Flowable<Color> f2 = createFlowable(1, TimeUnit.SECONDS, RED, BLUE, YELLOW);
 
 
         f1.zipWith(f2, (v1, v2) ->
@@ -49,21 +47,52 @@ public class App030ZipSimple {
         System.in.read();
     }
 
-    private static void zip002Merge() throws IOException {
-        Flowable<String> f1 = FlowFlowable.just("Peter", "Paul", "Marry", "Bob");
 
-        Flowable<String> f2 = Flowable.create(e -> {
-            for (int i = 0; i < 4; i++) {
-                e.onNext(Color.values()[ThreadLocalRandom.current().nextInt(4)].name());
-            }
-            e.onComplete();
-        }, BackpressureStrategy.MISSING);
+    private static void zip002Merge() throws IOException {
+
+
+        Flowable<String> f1 = createFlowable(1, TimeUnit.MILLISECONDS, "Peter", "Paul", "Marry", "Bob");
+        Flowable<Color> f2 = createFlowable(1, TimeUnit.MILLISECONDS, RED, BLUE, YELLOW);
 
 
         Flowable.merge(f1, f2).
-                blockingSubscribe(str -> logger.info("{}", str));
+                subscribe(str -> logger.info("{}", str));
 
         System.in.read();
+
+    }
+
+    private static void zip003Concat() throws IOException {
+
+
+        Flowable<String> f1 = createFlowable(1, TimeUnit.SECONDS, "Peter", "Paul", "Marry", "Bob");
+        Flowable<Color> f2 = createFlowable(1, TimeUnit.MILLISECONDS, RED, BLUE, YELLOW);
+
+
+        Flowable.concat(f1, f2).
+                subscribe(str -> logger.info("{}", str));
+
+        System.in.read();
+
+    }
+
+
+    private static <T> Flowable<T> createFlowable(long interval, TimeUnit timeUnit, T... data) {
+
+        return Flowable.<T>create(e -> {
+            try {
+                for (int i = 0; i < data.length; i++) {
+
+                    Thread.sleep(timeUnit.toMillis(interval));
+                    e.onNext(data[i]);
+
+                }
+            } catch (InterruptedException ex) {
+                e.onError(ex);
+            } finally {
+                e.onComplete();
+            }
+        }, BackpressureStrategy.MISSING).subscribeOn(Schedulers.io());
 
     }
 }
