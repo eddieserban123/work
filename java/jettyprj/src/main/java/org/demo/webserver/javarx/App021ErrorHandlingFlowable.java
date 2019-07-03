96e5a395907fa16b2fd365068a8233d9335bc636
Eddie Serban <eddie.serban@gmail.com>
2019-07-03 16:13:49 +0300

    add error handling

--- java/jettyprj/src/main/java/org/demo/webserver/javarx/App021ErrorHandlingFlowable.java
+++ java/jettyprj/src/main/java/org/demo/webserver/javarx/App021ErrorHandlingFlowable.java
@@ -13,7 +13,8 @@ public class App021ErrorHandlingFlowable {
     public static void main(String[] args) throws Exception {
         //throwException();
         //returnAvalueInsteadOfException();
-        signalErrorAndContinue();
+        //signalErrorAndContinue();
+        onErrorResume();
 
         System.in.read();
 
@@ -25,6 +26,9 @@ public class App021ErrorHandlingFlowable {
         Flowable<Integer> not56Stream = Flowable.just(3,4,8,22,56,7).
                 map(v-> {
                     if(v == 56) {
+
+                 
+
                             throw new RuntimeException(" Error, 56 found");
                     }
                     return v;
@@ -69,10 +73,35 @@ public class App021ErrorHandlingFlowable {
 
     }
 
+    /*
+    * returns a stream instead of an exception, useful for example to invoke a fallback
+    * method that returns an alternate Stream
+    * */
+    private static void onErrorResume() {
+        Flowable<Integer> not56Stream = Flowable.just(3,4,8,22,56,7,77).
+                flatMap(v-> doSomeAction(v).onErrorResumeNext(e-> {
+                    if(e instanceof IllegalArgumentException) {
+                        return Flowable.error(new RuntimeException("fatal errors, invalid argument"));
+                    }
+                    return Flowable.just(32,43);
+                })).
+                map(v->v+1);
+
+
+        not56Stream.subscribe(v-> logger.info("{}",v),
+                error -> logger.error("{}", error.getLocalizedMessage())
+        );
+
+    }
+
+
     private static Flowable<Integer> doSomeAction(Integer v) {
         return Flowable.create(emitter->{
             if(v == 56) {
                 throw new RuntimeException(" Error, 56 found");
+            }
+            if(v == 77) {
+                throw new IllegalArgumentException(" Error illegal arg, 77 found");
             }
             emitter.onNext(v);
 
