1ac02468878593e9b4bb9dc642cd7c48575f8b97
Eddie Serban <eddie.serban@gmail.com>
2019-08-06 17:06:38 +0300

    add more spring

--- java/jettyprj/src/main/java/org/demo/webserver/reactor/App010FirstExample.java
+++ java/jettyprj/src/main/java/org/demo/webserver/reactor/App010FirstExample.java
@@ -5,6 +5,7 @@ import org.reactivestreams.Subscription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public class App010FirstExample {
 
@@ -13,6 +14,8 @@ public class App010FirstExample {
       //  createSimpleExamplewithSimpleSubscriber();
       //  createSimpleExamplewithBakpressureSubscriber01();
       //  doSmthAndThrowAnError();
+       // textPlay1();
+        textPlay3();
 
     }
 
@@ -38,6 +41,38 @@ public class App010FirstExample {
                 ()->  logger.info(" done !"),
                 (subscription -> subscription.request(10L)));
 
+    }
+
+    private static void textPlay1() {
+        String words = "Amazingly few discotheques provide jukeboxe";
+
+        Flux<String> alphabet = Flux.fromArray(words.split("")).distinct().sort().zipWith(Flux.range(1,35), (letter,count) ->
+                String.format("%2d.  %s", count, letter));
+
+
+        alphabet.subscribe(v-> logger.info("{}",v));
+    }
+    private static void textPlay2() {
+        String words = "Amazingly few discotheques provide jukeboxe";
+
+        Flux<String> alphabet = Flux.fromArray(words.split("")).
+                distinct().concatWith(Mono.just("s")).
+                sort().
+                zipWith(Flux.range(1,35), (letter,count) ->
+                String.format("%2d.  %s", count, letter));
+
+
+        alphabet.subscribe(v-> logger.info("{}",v));
+    }
+
+    private static void textPlay3() {
+        String words = "Amazingly few discotheques provide jukeboxes";
+
+        Flux<String> alphabet = Flux.fromArray(words.split("")).
+                groupBy( String::toString).
+                flatMap(group -> Mono.zip(Mono.just(group.key()), group.count())).  //group.count().block expect onComplete!
+                map(keyAndCount -> keyAndCount.getT1() + " => " + keyAndCount.getT2() + "; ");
+        alphabet.subscribe(v-> logger.info("{}",v));
     }
 
 
@@ -81,5 +116,7 @@ public class App010FirstExample {
         ints.subscribe(i -> logger.info("{}", i),
                 error -> logger.error("Error: {} ", error.getLocalizedMessage()));
     }
+
+
 
 }
