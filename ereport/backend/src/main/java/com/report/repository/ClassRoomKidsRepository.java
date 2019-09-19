7c80fa8472d635e2d3c6b80c77f0976be74aef23
eddie <eddie.serban@gmail.com>
2019-09-19 18:17:19 +0300

    first kidsRepository implemenation

--- ereport/backend/src/main/java/com/report/repository/ClassRoomKidsRepository.java
+++ ereport/backend/src/main/java/com/report/repository/ClassRoomKidsRepository.java
@@ -1,13 +1,20 @@
 package com.report.repository;
 
+import com.datastax.driver.core.BatchStatement;
+import com.datastax.driver.core.BoundStatement;
 import com.datastax.driver.core.Session;
 import com.report.entity.classroomkids.ClassRoomKids;
 import com.report.entity.classroomkids.ClassRoomKidsKey;
 import org.springframework.data.cassandra.core.CassandraOperations;
+import org.springframework.data.cassandra.core.cql.support.CachedPreparedStatementCreator;
 import org.springframework.data.cassandra.core.cql.support.PreparedStatementCache;
 import org.springframework.data.cassandra.repository.query.CassandraEntityInformation;
 import org.springframework.data.cassandra.repository.support.SimpleCassandraRepository;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+
+import static com.datastax.driver.core.querybuilder.QueryBuilder.*;
 
 /*
 This repository is responsbile for changes (new kids coming, or kids leaving a classroom).
@@ -30,11 +37,35 @@ public class ClassRoomKidsRepository extends SimpleCassandraRepository<ClassRoom
         this.cassandraTemplate = cassandraTemplate;
     }
 
-    public void insertKidInClassRoom(String classRoomId, Integer year, Integer month, Integer day) {
-        
+    public void insertKidInClassRoom(String classRoomId, LocalDate snapShotDate, String personId) {
+        BatchStatement batch = new BatchStatement(BatchStatement.Type.LOGGED).add(insertKidInClassRoomQuery(classRoomId,snapShotDate,personId));
+
+        session.execute(batch);
     }
 
 
+    private BoundStatement insertKidInClassRoomQuery(String classRoomId, LocalDate snapShotDate, String personId) {
+        return CachedPreparedStatementCreator.of(
+                cache,
 
-  //  Mono<ClassRoomKids> findById(ClassRoomKidsKey id);
+                insertInto("classroom_kids").
+                        value("id_classroom", bindMarker("id_classroom")).
+                        value("snapshot_date", bindMarker("snapshot_date")).
+                        value("person_id", bindMarker("person_id"))
+        )
+                .createPreparedStatement(session)
+                .bind()
+                .setDate("snapshot_date", toCqlDate(snapShotDate))
+                .setString("id_classroom", classRoomId)
+                .setString("person_id", personId);
+
+
+    }
+
+
+    private com.datastax.driver.core.LocalDate toCqlDate(LocalDate date) {
+        return com.datastax.driver.core.LocalDate.fromYearMonthDay(
+                date.getYear(), date.getMonth().getValue(), date.getDayOfMonth());
+    }
+
 }
