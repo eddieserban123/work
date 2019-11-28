7929f1b576233ededb7861df16e93ec34102b5eb
eddie <eddie.serban@gmail.com>
2019-11-28 13:19:08 +0200

    add spring project to test ModelAttribute

---
+++ java/spring-boot/modelattribute/src/test/java/org/demomvc/modelattribute/ModelattributeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demomvc.modelattribute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelattributeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
