08253c5e14d613f0db6a1ec9d01f0e6746cca29b
Eduard Serban <eddie.serban@gmail.com>
2020-03-01 23:36:35 +0200

    adding etf

---
+++ java/bvbetf/src/main/java/com/etf/bvb/App.java
@@ -0,0 +1,13 @@
+package com.etf.bvb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
